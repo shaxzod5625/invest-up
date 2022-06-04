aaa15d044f178041c8df085868702ccf36ae6811
uubek <uubeks2@gmail.com>
2022-06-04 12:26:30 +0500

    update

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 /* axios.defaults.baseURL = "https://imaku.uz/api"; */
-axios.defaults.baseURL = "http://10.0.100.6:8000/api";
+axios.defaults.baseURL = "http://10.0.118.156:8000/api";
 
 axios.defaults.headers.common = {
     "Authorization": localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : ''
--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export default () => {
   return axios.create({
-    baseURL: 'http://10.0.100.6:8000/api',
+    baseURL: 'http://10.0.118.156:8000/api',
     headers: {
       "Authorization": localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : ''
     }
