562ad342824ebd451b2e51985559e7fd36bdb307
uubek <uubeks2@gmail.com>
2022-06-04 09:20:06 +0500

    updateurl

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 /* axios.defaults.baseURL = "https://imaku.uz/api"; */
-axios.defaults.baseURL = "http://192.168.0.157:8000/api";
+axios.defaults.baseURL = "http://10.0.100.6:8000/api";
 
 axios.defaults.headers.common = {
     "Authorization": localStorage.getItem('token') ? `Bearer ${localStorage.getItem('token')}` : ''
