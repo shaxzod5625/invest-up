d3e449a2ba5b69d2e35697b3367a7e54a576d696
Shaxzod <shaxzodzor007@gmail.com>
2022-06-05 13:05:38 +0500

    updated

--- src/components/section/ProfileSection.vue
+++ src/components/section/ProfileSection.vue
@@ -6,10 +6,40 @@
                         <AuthorSidebar :content="company.content"/>
                     </div><!-- end col-lg-8 -->
                 </div><!-- end row -->
-                <div class="col-xl-12 ps-xl-4">
+                <div>
                   <div class="row g-gs">
-                    <div class="col-xl-3 col-lg-4 col-sm-6" v-for="product in company.projects" :key="product.id">
-                        <Products :project="product"></Products>
+                    <div class="col-xl-3 col-lg-4 col-sm-6" v-for="project in company.projects" :key="project.id">
+                        <div class="card card-full">
+                          <router-link :to="`/item-details/${project.alias}`" class="">
+                            <div class="card-image">
+                              <img :src="project.image" class="card-img-top" alt="art image">
+                            </div>
+                          </router-link>
+                          <div class="card-body p-4">
+                              <router-link :to="`/item-details/${project.alias}`" class="">
+                                  <h5 class="card-title text-truncate mb-0">{{ project.title }}</h5>
+                              </router-link>
+                              <div class="card-author mb-1 d-flex align-items-center">
+                                  <span class="me-1 card-author-by">Кампания</span>
+                                  <div class="custom-tooltip-wrap">
+                                      <router-link :to="`/company/${project.company_alias}`" class="custom-tooltip author-link">
+                                          {{ project.company_title }}
+                                      </router-link>
+                                  </div><!-- end custom-tooltip-wrap -->
+                              </div><!-- end card-author -->
+                              <div class="card-price-wrap d-flex align-items-center justify-content-sm-between mb-3">
+                                  <div class="me-5 me-sm-2">
+                                      <span class="card-price-title">Цель</span>
+                                      <span class="card-price-number">&dollar; {{ project.target }}</span>
+                                  </div>
+                                  <div class="text-sm-end">
+                                      <span class="card-price-title">Собрано</span>
+                                      <span class="card-price-number">&dollar; {{ project.collected }}</span>
+                                  </div>
+                              </div><!-- end card-price-wrap -->
+                              <router-link :to="`/item-details/${project.alias}`" class="btn btn-sm btn-primary">Поддержать</router-link>
+                          </div><!-- end card-body -->
+                      </div>
                     </div>
                   </div>
                 </div><!-- end col-lg-8 -->
